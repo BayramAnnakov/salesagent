0754fc52e40bba3c552d9167e99c4a0d3b9a50c1
Bayram Annakov <bayram.annakov@gmail.com>
2024-03-15 23:21:59 +0000

    fetching zoom meeting transcript

--- agent.py
+++ agent.py
@@ -6,8 +6,17 @@ from llama_index.tools.google import GoogleCalendarToolSpec
 import requests
 import os
 
+from typing import Dict, Any
+
+from utils import get_zoom_token
+
 with open('bayram_linkedin_profile.txt','r') as file:
     profile = file.read()
+
+
+zoom_token = get_zoom_token()
+
+print(zoom_token)
 
 
 def search_linkedin(firstName: str, lastName: str, companyName: str) -> str:
@@ -36,8 +45,29 @@ with open('bad_sales_call.txt','r') as file:
     call_transcript = file.read()
 
 def get_meeting_transcript(meeting_id: str) -> str:
-    """Gets the transcript of a meeting with a given ID"""
+    """Gets the transcript of a meeting with a given Zoom meeting ID"""
     print(f"Getting transcript for meeting {meeting_id}")
+
+    meeting_id = "84625563686"
+
+    headers = {'Authorization': 'Bearer ' + zoom_token}
+
+    response = requests.get(f'https://api.zoom.us/v2/meetings/{meeting_id}/recordings', headers=headers)
+
+    """looks through all recording_files and finds the one with the transcript"""
+    recording_files = response.json().get('recording_files')
+
+    for recording_file in recording_files:
+        if recording_file.get('file_type') == 'TRANSCRIPT':
+            transcript_download_url = recording_file.get('download_url')
+            break
+    
+    print("Transcript download URL:", transcript_download_url)
+
+    response = requests.get(transcript_download_url, headers=headers)
+
+    print("Transcript:", response.text)
+
     return call_transcript
 
 meeting_transcript_tool = FunctionTool.from_defaults(fn=get_meeting_transcript)
@@ -49,12 +79,11 @@ llm = OpenAI(model="gpt-4-0125-preview")
 agent = OpenAIAgent.from_tools([linkedin_tool, meeting_transcript_tool, gcal_tools[0]], llm=llm, verbose=True, system_prompt="You are sales coach for a company that offers private jet services. You help sales managers to prepare for meetings, analyze their sales calls and provide feedback.")
 
 response = agent.chat("Search the upcoming sales calendar events on March 17th 2024.")
-
 print(str(response))
 
 response = agent.chat("Prepare a memo how to prepare for this private jet services sales call using info about the event participant from their LinkedIn profile. Score this lead's success probability from 1 to 10 based on the LinkedIn profile information and the upcoming sales call event details. List possible topics or questions to discuss/ask to make the sales call successful. ")
 print(str(response))
 
-response = agent.chat("Analyze the sales call and provide feedback. Score the sales call from 1 to 10.")
+response = agent.chat("Analyze the sales call using meeting transcript that is downloaded by zoom meeting id. Score the sales call from 1 to 10.")
 
 print(str(response))
---
+++ utils.py
@@ -0,0 +1,21 @@
+import os
+import requests
+
+from typing import Dict, Any
+
+def get_zoom_token() -> Dict[str, Any]:
+    """Gets the Zoom API token"""
+    account_id = os.environ['ZOOM_ACCOUNT_ID']
+    client_id = os.environ['ZOOM_CLIENT_ID']
+    client_secret = os.environ['ZOOM_CLIENT_SECRET']
+
+    params = {
+        "grant_type": "account_credentials",
+        "account_id": account_id,
+    }
+
+    response = requests.post('https://api.zoom.us/oauth/token', params=params, auth=(client_id, client_secret))
+
+    token = response.json().get('access_token')
+
+    return token
